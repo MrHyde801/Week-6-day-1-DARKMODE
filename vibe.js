4f921e29e2e7bf06590c46c91f159f5c863a68b4
Ian Hyde <ianhyde801@gmail.com>
2021-02-02 18:47:52 -0700

    dark mode engaged

---
+++ vibe.js
@@ -0,0 +1,95 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+let appTo = $('#BigContainer');
+storeItems.forEach(function(item) {
+    if(item.inStock) {
+        appTo.append(`
+        <div class="inStock">
+            <p>$${item.price}</p>
+            <p>${item.name}</p>
+            <p>${item.details}</p>
+        </div>`)
+    }
+})
+
+appTo.append('<button type="button" onclick="darkModeEngaged()">Toggle Dark Mode</button>')
+
+
+function darkModeEngaged() {
+    let boD = $('body');
+    let d2 = $('h1');
+    let inStockD = $('.inStock')
+    let d3 = $('button')
+
+    boD.toggleClass('dark1');
+    d2.toggleClass('dark2');
+    inStockD.toggleClass('inStockDark');
+    d3.toggleClass('dark3')
+}
